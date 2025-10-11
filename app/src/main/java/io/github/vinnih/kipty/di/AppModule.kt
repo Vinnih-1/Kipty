13ee6f809282955b5850d938a958c67ead8e0b09
Vinícius Albert <looucosad@gmail.com>
2025-10-10 23:25:11 -0300

    deps(hilt): implement hilt dependency injection

---
+++ app/src/main/java/io/github/vinnih/kipty/di/AppModule.kt
@@ -0,0 +1,27 @@
+package io.github.vinnih.kipty.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.github.vinnih.kipty.data.local.AppDatabase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun getDatabase(@ApplicationContext context: Context) = Room
+        .databaseBuilder(context, AppDatabase::class.java, "app_database")
+        .fallbackToDestructiveMigration(true)
+        .build()
+
+    @Provides
+    @Singleton
+    fun getTranscriptionDao(db: AppDatabase) = db.transcriptionDao()
+}

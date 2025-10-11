838ce5aa6c0b83dace10340895913cd45c2beed7
Vinícius Albert <looucosad@gmail.com>
2025-10-11 13:13:12 -0300

    style(transcription card): applying styles to title and description

--- app/src/main/java/io/github/vinnih/kipty/ui/components/KiptyTranscriptionItem.kt
+++ app/src/main/java/io/github/vinnih/kipty/ui/components/KiptyTranscriptionItem.kt
@@ -24,6 +24,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import io.github.vinnih.kipty.R
 import io.github.vinnih.kipty.data.local.entity.Transcription
@@ -75,16 +77,31 @@ fun KiptyTranscriptionItem(
                 )
             },
     ) {
-        Row {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(end = 8.dp),
+        ) {
             Icon(
                 modifier = Modifier.size(128.dp),
                 painter = painterResource(R.drawable.image_48px),
                 contentDescription = "No image found icon",
                 tint = MaterialTheme.colorScheme.onBackground
             )
-            Column(modifier = Modifier.padding(top = 14.dp)) {
-                Text(transcription.transcriptionName)
-                Text(transcription.transcriptionDescription!!)
+            Column(modifier = Modifier.padding(top = 14.dp, bottom = 14.dp)) {
+                Text(
+                    text = transcription.transcriptionName,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = transcription.transcriptionDescription!!,
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontWeight = FontWeight.Light,
+                    maxLines = 3,
+                    overflow = TextOverflow.Ellipsis
+                )
             }
         }
     }

0daa4f3a919b989e87465785600e73eeb5b654c7
Vinícius Albert <looucosad@gmail.com>
2025-10-11 12:28:41 -0300

    feat(transcription): feature to delete a transcription

--- app/src/main/java/io/github/vinnih/kipty/ui/components/KiptyTranscriptionItem.kt
+++ app/src/main/java/io/github/vinnih/kipty/ui/components/KiptyTranscriptionItem.kt
@@ -1,41 +1,68 @@
 package io.github.vinnih.kipty.ui.components
 
+import android.widget.Toast
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import io.github.vinnih.kipty.R
 import io.github.vinnih.kipty.data.local.entity.Transcription
+import io.github.vinnih.kipty.ui.home.HomeViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun KiptyTranscriptionItem(
     modifier: Modifier = Modifier,
     transcription: Transcription,
-    onClick: (Transcription) -> Unit = {}
+    viewModel: HomeViewModel,
 ) {
     var showBottomSheet by remember { mutableStateOf(false) }
 
     if (showBottomSheet) {
-        ModalBottomSheet(onDismissRequest = { showBottomSheet = false }) {
-            Text("Hi")
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            dragHandle = null
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalArrangement = Arrangement.spacedBy(12.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                TextButton(
+                    onClick = {
+                        viewModel.deleteTranscription(transcription)
+                        Toast.makeText(viewModel.context, R.string.delete_transcription_successful_toast, Toast.LENGTH_SHORT).show()
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(R.drawable.delete_48px),
+                        contentDescription = "Delete icon"
+                    )
+                }
+            }
         }
     }
     Card(
@@ -43,7 +70,9 @@ fun KiptyTranscriptionItem(
             .fillMaxWidth()
             .pointerInput(Unit) {
                 detectTapGestures(
-                    onTap = { onClick },
+                    onTap = {
+                        // TODO: Tap implementation
+                    },
                     onLongPress = { showBottomSheet = true }
                 )
             },

4e4ec8762416257d66b1f02148869a13eb604ead
Vinícius Albert <looucosad@gmail.com>
2025-10-11 12:28:16 -0300

    refactor: get application context from constructor

--- app/src/main/java/io/github/vinnih/kipty/ui/home/HomeViewModel.kt
+++ app/src/main/java/io/github/vinnih/kipty/ui/home/HomeViewModel.kt
@@ -1,8 +1,10 @@
 package io.github.vinnih.kipty.ui.home
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.github.vinnih.kipty.data.local.entity.Transcription
 import io.github.vinnih.kipty.data.local.repository.TranscriptionRepository
 import kotlinx.coroutines.flow.SharingStarted
@@ -12,7 +14,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    val transcriptionRepository: TranscriptionRepository
+    val transcriptionRepository: TranscriptionRepository,
+    @ApplicationContext val context: Context
 ) : ViewModel() {
 
     val transcriptions = transcriptionRepository.getAllTranscriptions()

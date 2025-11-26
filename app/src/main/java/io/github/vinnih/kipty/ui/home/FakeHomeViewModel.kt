3836b5e6c9625eef59d4beeb8678c3f4f3e1aa46
Vinícius Albert <looucosad@gmail.com>
2025-11-26 15:30:17 -0300

    init: re-start kipty development

--- app/src/main/java/io/github/vinnih/kipty/ui/home/FakeHomeViewModel.kt
+++
@@ -1,9 +0,0 @@
-package io.github.vinnih.kipty.ui.home
-
-import java.io.File
-
-class FakeHomeViewModel : HomeUiController {
-    override fun convertFile(file: File, onSuccess: (File) -> Unit) {
-        TODO("Not yet implemented")
-    }
-}
--- app/src/main/java/io/github/vinnih/kipty/ui/home/HomeUiController.kt
+++
@@ -1,8 +0,0 @@
-package io.github.vinnih.kipty.ui.home
-
-import java.io.File
-
-interface HomeUiController {
-
-    fun convertFile(file: File, onSuccess: (File) -> Unit)
-}

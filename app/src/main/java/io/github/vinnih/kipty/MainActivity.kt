9f80ebc921259ba4139573ad801c91e67e0de7b0
Vinícius Albert <looucosad@gmail.com>
2025-12-01 17:05:08 -0300

    fix: set Home as the first screen in MainActivity

--- app/src/main/java/io/github/vinnih/kipty/MainActivity.kt
+++ app/src/main/java/io/github/vinnih/kipty/MainActivity.kt
@@ -4,18 +4,52 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import io.github.vinnih.kipty.data.application.AppConfig
+import io.github.vinnih.kipty.ui.components.FloatingAddButton
+import io.github.vinnih.kipty.ui.components.KiptyBottomBar
+import io.github.vinnih.kipty.ui.components.KiptyTopBar
+import io.github.vinnih.kipty.ui.home.HomeScreen
+import io.github.vinnih.kipty.ui.home.HomeViewModel
 import io.github.vinnih.kipty.ui.theme.AppTheme
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        if (!AppConfig(applicationContext).read().defaultSamplesLoaded) {
+            lifecycleScope.launch {
+                homeViewModel.copyAssets()
+            }
+        }
+
         setContent {
             AppTheme {
+                Scaffold(
+                    topBar = { KiptyTopBar("Home") },
+                    bottomBar = { KiptyBottomBar() },
+                    floatingActionButton = {
+                        FloatingAddButton(onClick = {}, modifier = Modifier.size(72.dp))
+                    }
+                ) { paddingValues ->
+                    HomeScreen(
+                        controller = homeViewModel,
+                        modifier = Modifier.padding(paddingValues)
+                    )
+                }
             }
         }
     }

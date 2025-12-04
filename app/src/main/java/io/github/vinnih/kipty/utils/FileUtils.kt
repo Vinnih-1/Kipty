08f5bc64036f93bce7c7e4058b46297a29aa7bef
Vinícius Albert <looucosad@gmail.com>
2025-12-04 14:16:37 -0300

    chore: rename from FileContentPicker to FileUtils

---
+++ app/src/main/java/io/github/vinnih/kipty/utils/FileUtils.kt
@@ -0,0 +1,62 @@
+package io.github.vinnih.kipty.utils
+
+import android.content.Context
+import android.net.Uri
+import io.github.nailik.androidresampler.Resampler
+import io.github.nailik.androidresampler.ResamplerConfiguration
+import io.github.nailik.androidresampler.data.ResamplerChannel
+import io.github.nailik.androidresampler.data.ResamplerQuality
+import io.github.vinnih.androidtranscoder.utils.toWavReader
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+fun Uri.copyFile(context: Context): File {
+    val inputStream = context.contentResolver.openInputStream(this)!!
+    val file = File(context.filesDir, "")
+    inputStream.use { input ->
+        file.outputStream().use { output -> input.copyTo(output) }
+    }
+    return file
+}
+
+fun resample(channels: Int, inSampleRate: Int, outSampleRate: Int, pcmData: ByteArray): ByteArray {
+    val resamplerConfiguration = ResamplerConfiguration(
+        quality = ResamplerQuality.BEST,
+        inputChannel = if (channels == 1) ResamplerChannel.MONO else ResamplerChannel.STEREO,
+        inputSampleRate = inSampleRate,
+        outputChannel = ResamplerChannel.MONO,
+        outputSampleRate = outSampleRate
+    )
+    val resampler = Resampler(resamplerConfiguration)
+
+    return resampler.resample(pcmData)
+}
+fun File.toFloatArray(context: Context): FloatArray {
+    val reader = this.toWavReader(context.cacheDir)
+    val baos = ByteArrayOutputStream()
+    reader.data.inputStream().use { it.copyTo(baos) }
+
+    val fullData = baos.toByteArray()
+    val buffer = ByteBuffer.wrap(fullData)
+    buffer.order(ByteOrder.LITTLE_ENDIAN)
+    buffer.position(44)
+
+    val pcmDataLength = buffer.limit() - buffer.position()
+    val pcmData = ByteArray(pcmDataLength)
+    buffer.get(pcmData, 0, pcmDataLength)
+
+    val resampledPcm = resample(reader.channels, reader.sampleRate, 16000, pcmData)
+    val finalFloatArray = FloatArray(resampledPcm.size / 2)
+    val shortBuffer = ByteBuffer.wrap(resampledPcm)
+        .order(ByteOrder.LITTLE_ENDIAN)
+        .asShortBuffer()
+
+    for (i in 0 until shortBuffer.limit()) {
+        val shortValue = shortBuffer.get(i).toInt()
+        finalFloatArray[i] = (shortValue / 32767.0f).coerceIn(-1f..1f)
+    }
+
+    return finalFloatArray
+}

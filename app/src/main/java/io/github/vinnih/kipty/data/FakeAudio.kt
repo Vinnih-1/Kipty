7c64f659db3723f318491b975ce9e8d36e437402
Vinícius Albert <looucosad@gmail.com>
2025-12-14 10:48:48 -0300

    chore: implement fake methods

--- app/src/main/java/io/github/vinnih/kipty/data/FakeAudio.kt
+++ app/src/main/java/io/github/vinnih/kipty/data/FakeAudio.kt
@@ -199,4 +199,202 @@ object FakeAudio {
             ]
         }
     """.trimIndent()
+
+    @Suppress("ktlint:standard:property-naming")
+    var audio_1888_11_13 = """
+        {
+            "uid": 6,
+            "name": "1888-11-13_sircharleswarren",
+            "duration": 0,
+            "path": "",
+            "createdAt": "2025-12-06T19:16:50.373186",
+            "transcription": [
+                {
+                    "start": 0,
+                    "end": 9440,
+                    "text": ":  From Washington, abolition of slavery, from the New York Times, February 1st, 1865,"
+                },
+                {
+                    "start": 9440,
+                    "end": 14280,
+                    "text": ":  read for LibraVox.org by Betsy Bush."
+                },
+                {
+                    "start": 14280,
+                    "end": 20480,
+                    "text": ":  Passage of the Constitutional Amendment, 119 Yays Against 56 Nays,"
+                },
+                {
+                    "start": 20480,
+                    "end": 25000,
+                    "text": ":  exciting scene in the House, enthusiasm over the result."
+                },
+                {
+                    "start": 25000,
+                    "end": 28960,
+                    "text": ":  Washington, Tuesday, January 31st."
+                },
+                {
+                    "start": 28960,
+                    "end": 32240,
+                    "text": ":  The passage of the Constitutional Amendment."
+                },
+                {
+                    "start": 32240,
+                    "end": 37560,
+                    "text": ":  The great feature of the existing rebellion was the passage today by the House of Representatives"
+                },
+                {
+                    "start": 37560,
+                    "end": 42000,
+                    "text": ":  of the Resolutions submitting to the legislatures of the several states and"
+                },
+                {
+                    "start": 42000,
+                    "end": 45760,
+                    "text": ":  amendment to the Constitution abolishing slavery."
+                },
+                {
+                    "start": 45760,
+                    "end": 50040,
+                    "text": ":  It was an epoch in the history of the country and will be remembered by the members of the"
+                },
+                {
+                    "start": 50040,
+                    "end": 54240,
+                    "text": ":  House and Spectators present as an event in their lives."
+                },
+                {
+                    "start": 54240,
+                    "end": 60000,
+                    "text": ":  At 3 o'clock by General Consent, all discussion having ceased, the preliminary votes"
+                },
+                {
+                    "start": 60000,
+                    "end": 66400,
+                    "text": ":  to reconsider and second the demand for the previous question were agreed to by a vote of 113"
+                },
+                {
+                    "start": 66400,
+                    "end": 69080,
+                    "text": ":  Yays to 58 Nays."
+                },
+                {
+                    "start": 69080,
+                    "end": 74880,
+                    "text": ":  And amid profound silence, the speaker announced that the Yays and Nays would be taken directly"
+                },
+                {
+                    "start": 74880,
+                    "end": 77680,
+                    "text": ":  upon the pending proposition."
+                },
+                {
+                    "start": 77680,
+                    "end": 82920,
+                    "text": ":  During the call, when prominent Democrats voted I, there was a suppressed evidence of"
+                },
+                {
+                    "start": 82920,
+                    "end": 88480,
+                    "text": ":  applause and gratification exhibited in the galleries, but it was evident that the great"
+                },
+                {
+                    "start": 88480,
+                    "end": 92400,
+                    "text": ":  interest centered entirely upon the final result."
+                },
+                {
+                    "start": 92400,
+                    "end": 98480,
+                    "text": ":  And when the presiding officer announced that the resolution was agreed to by Yays 119"
+                },
+                {
+                    "start": 98480,
+                    "end": 105800,
+                    "text": ":  Nays 56, the enthusiasm of all present save a few disappointed politicians knew no bounds"
+                },
+                {
+                    "start": 105800,
+                    "end": 110640,
+                    "text": ":  and for several moments the scene was grand and impressive beyond description."
+                },
+                {
+                    "start": 110640,
+                    "end": 115760,
+                    "text": ":  No attempt was made to suppress the applause which came from all sides, everyone feeling"
+                },
+                {
+                    "start": 115760,
+                    "end": 121720,
+                    "text": ":  that the occasion justified the fullest expression of approbation and joy."
+                },
+                {
+                    "start": 121720,
+                    "end": 124960,
+                    "text": ":  End of article."
+                },
+                {
+                    "start": 124960,
+                    "end": 131200,
+                    "text": ":  This recording is in the public domain."
+                },
+                {
+                    "start": 131200,
+                    "end": 135600,
+                    "text": ":  And still thinking it's something extraordinary that the sun should rise so early, I looked"
+                },
+                {
+                    "start": 135600,
+                    "end": 141400,
+                    "text": ":  into the almanac where I found it to be the hour given for his rising on that day."
+                },
+                {
+                    "start": 141400,
+                    "end": 146320,
+                    "text": ":  I looked forward to and found he was to rise still earlier every day till towards the end"
+                },
+                {
+                    "start": 146320,
+                    "end": 151480,
+                    "text": ":  of June and that at no time in the year he retarded his rising so long as till eight"
+                },
+                {
+                    "start": 151480,
+                    "end": 153880,
+                    "text": ":  o'clock."
+                },
+                {
+                    "start": 153880,
+                    "end": 159040,
+                    "text": ":  Your readers, who with me have never seen any signs of sunshine before noon and seldom"
+                },
+                {
+                    "start": 159040,
+                    "end": 163520,
+                    "text": ":  regard the astronomical part of the almanac, will be as much as Stannis just I was when"
+                },
+                {
+                    "start": 163520,
+                    "end": 169120,
+                    "text": ":  the hear of his rising so early and especially when I assure them that he gives light as"
+                },
+                {
+                    "start": 169120,
+                    "end": 171480,
+                    "text": ":  soon as he rises."
+                },
+                {
+                    "start": 171480,
+                    "end": 173320,
+                    "text": ":  I am convinced of this."
+                },
+                {
+                    "start": 173320,
+                    "end": 180240,
+                    "text": ":  I am certain of my"
+                }
+            ]
+        }        
+    """.trimIndent()
 }
--- app/src/main/java/io/github/vinnih/kipty/ui/audio/FakeAudioViewModel.kt
+++ app/src/main/java/io/github/vinnih/kipty/ui/audio/FakeAudioViewModel.kt
@@ -1,18 +1,19 @@
 package io.github.vinnih.kipty.ui.audio
 
+import androidx.work.WorkInfo
 import io.github.vinnih.kipty.data.database.entity.AudioEntity
 import io.github.vinnih.kipty.data.database.entity.AudioTranscription
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 class FakeAudioViewModel : AudioController {
-    override val isTranscribing: StateFlow<Boolean>
+    override val audioUiState: StateFlow<AudioEntity?>
         get() = TODO("Not yet implemented")
 
-    override fun convertTranscription(transcribedData: String): List<AudioTranscription> {
-        TODO("Not yet implemented")
-    }
-
-    override fun transcribeAudio(audioEntity: AudioEntity, onSuccess: (AudioEntity) -> Unit) {
+    override fun transcribeAudio(
+        audioEntity: AudioEntity,
+        onSuccess: (List<AudioTranscription>) -> Unit
+    ) {
         TODO("Not yet implemented")
     }
 
@@ -20,7 +21,19 @@ class FakeAudioViewModel : AudioController {
         TODO("Not yet implemented")
     }
 
+    override fun observeTranscriptionWork(): Flow<List<WorkInfo>> {
+        TODO("Not yet implemented")
+    }
+
+    override fun cancelTranscriptionWork(audioEntity: AudioEntity) {
+        TODO("Not yet implemented")
+    }
+
     override suspend fun getById(id: Int): AudioEntity {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getCurrent(id: Int) {
         TODO("Not yet implemented")
     }
 }

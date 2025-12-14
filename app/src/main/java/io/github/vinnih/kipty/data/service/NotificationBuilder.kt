3dcd346e1762e8e7574507085af865fc7875bfd4
Vinícius Albert <looucosad@gmail.com>
2025-12-14 10:48:26 -0300

    chore: remove unnecessary if

--- app/src/main/java/io/github/vinnih/kipty/data/service/NotificationBuilder.kt
+++ app/src/main/java/io/github/vinnih/kipty/data/service/NotificationBuilder.kt
@@ -4,7 +4,6 @@ import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import io.github.vinnih.kipty.R
 
@@ -12,22 +11,21 @@ const val NOTIFICATION_ID = 2122
 const val CHANNEL_ID = "kipty_transcription_running"
 
 fun createNotificationChannel(context: Context) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        val name = "Initializing transcription"
-        val description = "Your transcription is running, please wait."
+    val name = "Initializing transcription"
+    val description = "Your transcription is running, please wait."
 
-        val channel = NotificationChannel(
-            CHANNEL_ID,
-            name,
-            NotificationManager.IMPORTANCE_LOW
-        ).apply {
-            this.description = description
-        }
-        val notificationManager = context.getSystemService(
-            Context.NOTIFICATION_SERVICE
-        ) as NotificationManager
-        notificationManager.createNotificationChannel(channel)
+    val channel = NotificationChannel(
+        CHANNEL_ID,
+        name,
+        NotificationManager.IMPORTANCE_HIGH
+    ).apply {
+        this.description = description
     }
+    val notificationManager = context.getSystemService(
+        Context.NOTIFICATION_SERVICE
+    ) as NotificationManager
+
+    notificationManager.createNotificationChannel(channel)
 }
 
 fun createNotification(context: Context, ongoing: Boolean, text: String): Notification {
@@ -36,7 +34,7 @@ fun createNotification(context: Context, ongoing: Boolean, text: String): Notifi
     return NotificationCompat.Builder(context, CHANNEL_ID)
         .setContentTitle("Transcription")
         .setContentText(text)
-        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setSmallIcon(R.mipmap.ic_launcher_round)
         .setOngoing(ongoing)
         .setProgress(100, 0, true)
         .build()

554083ef3d66d84481931058960adbe9823b53c6
Vinícius Albert <looucosad@gmail.com>
2025-12-17 12:44:39 -0300

    feat: notification card to request permission

--- app/src/main/java/io/github/vinnih/kipty/ui/home/HomeScreen.kt
+++ app/src/main/java/io/github/vinnih/kipty/ui/home/HomeScreen.kt
@@ -1,7 +1,11 @@
 package io.github.vinnih.kipty.ui.home
 
+import android.Manifest
 import android.content.res.Configuration
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,6 +16,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -20,10 +26,19 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
 import io.github.vinnih.kipty.R
 import io.github.vinnih.kipty.data.database.entity.AudioEntity
 import io.github.vinnih.kipty.ui.audio.AudioController
@@ -32,19 +47,33 @@ import io.github.vinnih.kipty.ui.components.AudioCard
 import io.github.vinnih.kipty.ui.notification.NotificationController
 import io.github.vinnih.kipty.ui.theme.AppTheme
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalPermissionsApi::class)
 @Composable
 fun HomeScreen(
+    homeController: HomeController,
     audioController: AudioController,
     onClick: (AudioEntity) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val audioState = audioController.allAudios.collectAsState()
+    var notificationWarn by remember { mutableStateOf(true) }
 
-    LazyColumn(modifier = modifier.fillMaxSize().padding(top = 20.dp)) {
-        items(audioState.value) { audioData ->
-            AudioCard(audioEntity = audioData, onClick = {
-                onClick(audioData)
-            }, modifier = Modifier.fillMaxWidth().height(128.dp))
+    Column(modifier = modifier.fillMaxSize()) {
+        if (notificationWarn) {
+            NotificationPermissionWarn(
+                onEnable = {
+                    homeController.openNotificationSettings()
+                },
+                onDismiss = { notificationWarn = false }
+            )
+        }
+
+        LazyColumn {
+            items(audioState.value) { audioData ->
+                AudioCard(audioEntity = audioData, onClick = {
+                    onClick(audioData)
+                }, modifier = Modifier.fillMaxWidth().height(128.dp))
+            }
         }
     }
 }
@@ -57,9 +86,9 @@ fun HomeTopBar(
     onCreateClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val notificationState = notificationController.unreadNotifications.collectAsState()
     val colors = MaterialTheme.colorScheme
     val typography = MaterialTheme.typography
+    val notificationState = notificationController.unreadNotifications.collectAsState()
 
     TopAppBar(
         title = {
@@ -101,6 +130,67 @@ fun HomeTopBar(
     )
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+private fun NotificationPermissionWarn(onEnable: () -> Unit, onDismiss: () -> Unit) {
+    val permission = rememberPermissionState(Manifest.permission.POST_NOTIFICATIONS)
+
+    if (permission.status.isGranted) return onDismiss()
+
+    val colors = MaterialTheme.colorScheme
+    val typography = MaterialTheme.typography
+
+    Row(
+        modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp, vertical = 32.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth().height(86.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = colors.secondaryContainer.copy(alpha = 0.3f),
+                contentColor = colors.onSecondaryContainer
+            )
+        ) {
+            Box(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+                Icon(
+                    painter = painterResource(R.drawable.notifications),
+                    contentDescription = "Notification icon",
+                    modifier = Modifier.size(36.dp).align(Alignment.TopStart)
+                )
+                Column(
+                    modifier = Modifier.align(
+                        Alignment.Center
+                    ).padding(horizontal = 48.dp)
+                ) {
+                    Text(
+                        text = "Receive transcription notifications",
+                        style = typography.bodyMedium,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Be notified when transcription are ready",
+                        style = typography.bodySmall,
+                        fontWeight = FontWeight.Light
+                    )
+                    Text(
+                        text = "Enable",
+                        style = typography.bodySmall,
+                        fontWeight = FontWeight.Bold,
+                        color = colors.primary,
+                        modifier = Modifier.padding(top = 8.dp).clickable(onClick = onEnable)
+                    )
+                }
+                Icon(
+                    painter = painterResource(R.drawable.close),
+                    contentDescription = null,
+                    modifier = Modifier.size(
+                        24.dp
+                    ).align(Alignment.TopEnd).clickable(onClick = onDismiss)
+                )
+            }
+        }
+    }
+}
+
 @Preview(
     name = "Light",
     showBackground = true,
@@ -114,6 +204,7 @@ fun HomeTopBar(
 @Composable
 private fun HomeScreenPreview() {
     AppTheme {
-        HomeScreen(audioController = FakeAudioViewModel(), onClick = {}, modifier = Modifier)
+        HomeScreen(homeController = FakeHomeViewModel(), audioController = FakeAudioViewModel(), onClick = {
+        }, modifier = Modifier)
     }
 }

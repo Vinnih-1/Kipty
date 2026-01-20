ec2d811d361cf421872d1828f68049511095f63b
Vinícius Albert <looucosad@gmail.com>
2026-01-20 11:33:59 -0300

    style: improve mini player bottom bar ui

--- app/src/main/java/io/github/vinnih/kipty/ui/player/PlayerScreen.kt
+++ app/src/main/java/io/github/vinnih/kipty/ui/player/PlayerScreen.kt
@@ -2,14 +2,19 @@ package io.github.vinnih.kipty.ui.player
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.BottomSheetScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -29,6 +34,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,8 +44,12 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.ui.compose.state.rememberNextButtonState
 import androidx.media3.ui.compose.state.rememberPlayPauseButtonState
+import androidx.media3.ui.compose.state.rememberPreviousButtonState
+import coil3.compose.AsyncImage
 import io.github.vinnih.kipty.R
+import io.github.vinnih.kipty.ui.components.BaseButton
 import io.github.vinnih.kipty.ui.components.TextViewer
 import io.github.vinnih.kipty.ui.configuration.ConfigurationController
 import io.github.vinnih.kipty.ui.configuration.FakeConfigurationViewModel
@@ -60,6 +72,7 @@ fun PlayerScreen(
     val playerUiState by playerController.uiState.collectAsState()
     val configurationUiState by configurationController.uiState.collectAsState()
     var visible by remember { mutableStateOf(true) }
+    val scope = rememberCoroutineScope()
 
     LaunchedEffect(scaffoldState.bottomSheetState) {
         snapshotFlow {
@@ -72,14 +85,19 @@ fun PlayerScreen(
     Column(modifier = modifier.fillMaxSize().background(color = colors.surfaceContainer)) {
         AnimatedVisibility(visible) {
             MiniPlayer(
-                scaffoldState = scaffoldState,
+                onExpand = {
+                    scope.launch { scaffoldState.bottomSheetState.expand() }
+                },
+                player = playerController.player,
                 playerUiState = playerUiState,
                 peekHeight = peekHeight
             )
         }
         AnimatedVisibility(!visible) {
             Player(
-                scaffoldState = scaffoldState,
+                onCollapse = {
+                    scope.launch { scaffoldState.bottomSheetState.partialExpand() }
+                },
                 player = playerController.player,
                 peekHeight = peekHeight
             ) {
@@ -100,14 +118,13 @@ fun PlayerScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun Player(
-    scaffoldState: BottomSheetScaffoldState,
+    onCollapse: () -> Unit,
     player: Player,
     peekHeight: Dp,
     modifier: Modifier = Modifier,
     content: @Composable () -> Unit
 ) {
     val playPause = rememberPlayPauseButtonState(player)
-    val scope = rememberCoroutineScope()
 
     Column(modifier = modifier) {
         Row(
@@ -118,9 +135,7 @@ private fun Player(
                 .height(peekHeight)
                 .padding(top = 24.dp, start = 16.dp, end = 16.dp)
         ) {
-            IconButton(onClick = {
-                scope.launch { scaffoldState.bottomSheetState.partialExpand() }
-            }) {
+            IconButton(onClick = onCollapse) {
                 Icon(
                     painter = painterResource(R.drawable.keyboard_arrow_down),
                     contentDescription = "Dismiss player screen modal"
@@ -142,48 +157,128 @@ private fun Player(
     }
 }
 
+@androidx.annotation.OptIn(UnstableApi::class)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MiniPlayer(
-    scaffoldState: BottomSheetScaffoldState,
+    onExpand: () -> Unit,
+    player: Player,
     playerUiState: PlayerUiState,
     peekHeight: Dp,
     modifier: Modifier = Modifier
 ) {
     val colors = MaterialTheme.colorScheme
     val typography = MaterialTheme.typography
-    val scope = rememberCoroutineScope()
+    val playPause = rememberPlayPauseButtonState(player)
+    val next = rememberNextButtonState(player)
+    val previous = rememberPreviousButtonState(player)
+    val time = (playerUiState.progress * playerUiState.duration).toLong()
+    val image = if (playerUiState.audioEntity?.isDefault == true) {
+        "file:///android_asset/${playerUiState.audioEntity.imagePath}"
+    } else if (playerUiState.audioEntity != null) {
+        File(playerUiState.audioEntity.imagePath)
+    } else {
+        ""
+    }
 
     Column(modifier = modifier) {
-        LinearProgressIndicator(progress = {
-            playerUiState.progress
-        }, drawStopIndicator = {}, modifier = Modifier.fillMaxWidth())
+        LinearProgressIndicator(
+            progress = {
+                playerUiState.progress
+            },
+            drawStopIndicator = {},
+            gapSize = 0.dp,
+            modifier = Modifier.fillMaxWidth()
+        )
         Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(peekHeight)
-                .clickable {
-                    scope.launch { scaffoldState.bottomSheetState.expand() }
-                }
+                .clickable { onExpand.invoke() }
                 .padding(horizontal = 16.dp),
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(
-                text = playerUiState.audioEntity?.name ?: "Nothing playing",
-                style = typography.titleMedium,
-                color = colors.primary,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.fillMaxWidth(.7f)
-            )
-            Text(
-                text = "${
-                    (playerUiState.progress * playerUiState.duration).toLong().formatTime()
-                } / ${playerUiState.duration.formatTime()}",
-                style = typography.bodySmall,
-                color = colors.primary
-            )
+            Box(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    modifier = Modifier
+                        .align(Alignment.CenterStart)
+                        .fillMaxWidth(.65f)
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .clip(MaterialTheme.shapes.medium)
+                            .height(64.dp)
+                            .width(64.dp)
+                            .background(Color.Gray)
+                    ) {
+                        AsyncImage(
+                            model = image,
+                            contentDescription = null,
+                            contentScale = ContentScale.FillBounds,
+                            modifier = Modifier.width(64.dp).height(64.dp)
+                        )
+                    }
+                    Column {
+                        Text(
+                            text = playerUiState.audioEntity?.name ?: "Nothing playing",
+                            style = typography.titleMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
+                            modifier = Modifier.basicMarquee(iterations = Int.MAX_VALUE)
+                        )
+                        Text(
+                            text = "${time.formatTime()} / ${playerUiState.duration.formatTime()}",
+                            style = typography.bodySmall,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    }
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier.align(Alignment.CenterEnd)
+                ) {
+                    BaseButton(onClick = { previous.onClick() }, modifier = Modifier.size(32.dp)) {
+                        Icon(
+                            painter = painterResource(R.drawable.skip_previous),
+                            contentDescription = null,
+                            tint = colors.onBackground.copy(alpha = .6f),
+                            modifier = Modifier.size(32.dp)
+                        )
+                    }
+
+                    BaseButton(
+                        onClick = { playPause.onClick() },
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .size(48.dp)
+                            .background(color = colors.secondaryContainer.copy(.6f))
+                    ) {
+                        Icon(
+                            painter = painterResource(
+                                if (playPause.showPlay) R.drawable.play else R.drawable.pause
+                            ),
+                            contentDescription = null,
+                            tint = colors.onBackground.copy(alpha = .6f),
+                            modifier = Modifier.size(38.dp)
+                        )
+                    }
+                    BaseButton(onClick = { next.onClick() }, modifier = Modifier.size(32.dp)) {
+                        Icon(
+                            painter = painterResource(R.drawable.skip_next),
+                            contentDescription = null,
+                            tint = colors.onBackground.copy(alpha = .6f),
+                            modifier = Modifier.size(32.dp)
+                        )
+                    }
+                }
+            }
         }
     }
 }

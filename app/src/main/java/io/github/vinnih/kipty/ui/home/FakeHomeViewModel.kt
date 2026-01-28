0ffff6b5e933ce5a1ba1247882d414bbbfada714
Vinícius Albert <looucosad@gmail.com>
2026-01-28 03:02:29 -0300

    chore: fix home screen preview

--- app/src/main/java/io/github/vinnih/kipty/ui/home/FakeHomeViewModel.kt
+++ app/src/main/java/io/github/vinnih/kipty/ui/home/FakeHomeViewModel.kt
@@ -1,25 +1,32 @@
 package io.github.vinnih.kipty.ui.home
 
+import io.github.vinnih.kipty.data.FakeAudioData
+import io.github.vinnih.kipty.data.database.entity.AudioEntity
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.serialization.json.Json
 
 class FakeHomeViewModel : HomeController {
-    override val homeUiState: StateFlow<HomeUiState>
-        get() = TODO("Not yet implemented")
+
+    private val json = Json { ignoreUnknownKeys = true }
+
+    private val fakeAudioList: List<AudioEntity> = listOf(
+        json.decodeFromString(FakeAudioData.audio_1865_02_01),
+        json.decodeFromString(FakeAudioData.audio_1888_11_13)
+    )
+
+    override val homeUiState: StateFlow<HomeUiState> =
+        MutableStateFlow(HomeUiState(audioList = fakeAudioList, isLoading = false))
 
     override fun getPlayTimeById(id: Int): Flow<Long> {
-        TODO("Not yet implemented")
+        return flowOf(fakeAudioList.find { it.uid == id }?.playTime ?: 0L)
     }
 
-    override fun loadAudios() {
-        TODO("Not yet implemented")
-    }
+    override fun loadAudios() {}
 
-    override fun openNotificationSettings() {
-        TODO("Not yet implemented")
-    }
+    override fun openNotificationSettings() {}
 
-    override suspend fun populateDatabase(onSuccess: () -> Unit) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun populateDatabase(onSuccess: () -> Unit) {}
 }

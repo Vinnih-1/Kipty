7ece4f863f84fe9d98ef142be29910a1c43e5179
Vinícius Albert <looucosad@gmail.com>
2026-01-28 02:51:23 -0300

    chore: remove TODO comments to fix audio screen preview

--- app/src/main/java/io/github/vinnih/kipty/ui/audio/FakeAudioViewModel.kt
+++ app/src/main/java/io/github/vinnih/kipty/ui/audio/FakeAudioViewModel.kt
@@ -1,35 +1,34 @@
 package io.github.vinnih.kipty.ui.audio
 
+import io.github.vinnih.kipty.data.FakeAudioData
 import io.github.vinnih.kipty.data.database.entity.AudioEntity
+import io.github.vinnih.kipty.json
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
 
 class FakeAudioViewModel : AudioController {
 
     override val uiState: StateFlow<AudioUiState>
-        get() = TODO("Not yet implemented")
+        get() = MutableStateFlow(AudioUiState(canTranscribe = true))
 
-    override fun transcribeAudio(audioEntity: AudioEntity) {
-        TODO("Not yet implemented")
-    }
+    override fun transcribeAudio(audioEntity: AudioEntity) {}
 
     override fun getFlowById(id: Int): Flow<AudioEntity?> {
-        TODO("Not yet implemented")
+        val audioEntity = json.decodeFromString<AudioEntity>(FakeAudioData.audio_1888_11_13)
+        return flow { emit(audioEntity) }
     }
 
     override suspend fun saveAudio(audioEntity: AudioEntity): Long {
-        TODO("Not yet implemented")
+        return 1
     }
 
     override suspend fun getById(id: Int): AudioEntity? {
-        TODO("Not yet implemented")
+        return null
     }
 
-    override fun deleteAudio(audioEntity: AudioEntity) {
-        TODO("Not yet implemented")
-    }
+    override fun deleteAudio(audioEntity: AudioEntity) {}
 
-    override fun cancelTranscriptionWork(audioEntity: AudioEntity) {
-        TODO("Not yet implemented")
-    }
+    override fun cancelTranscriptionWork(audioEntity: AudioEntity) {}
 }

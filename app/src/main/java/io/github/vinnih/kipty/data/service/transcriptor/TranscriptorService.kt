6480d774dfda49cb239e6f7aaaeb50979c8a4ece
Vinícius Albert <looucosad@gmail.com>
2026-02-08 18:50:42 -0300

    refactor: move transcription process to transcription service

--- app/src/main/java/io/github/vinnih/kipty/data/service/transcriptor/TranscriptorService.kt
+++ app/src/main/java/io/github/vinnih/kipty/data/service/transcriptor/TranscriptorService.kt
@@ -5,9 +5,14 @@ import com.whispercpp.whisper.WhisperContext
 import dagger.hilt.android.qualifiers.ApplicationContext
 import io.github.vinnih.kipty.data.database.entity.AudioEntity
 import io.github.vinnih.kipty.data.database.entity.AudioTranscription
+import io.github.vinnih.kipty.data.service.AudioResampler
+import io.github.vinnih.kipty.data.service.AudioResampler.resample
 import io.github.vinnih.kipty.utils.convertTranscription
-import io.github.vinnih.kipty.utils.processAudioSegments
+import io.github.vinnih.kipty.utils.moveTo
 import java.io.File
+import java.io.FileInputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlinx.coroutines.Dispatchers
@@ -77,4 +82,102 @@ class TranscriptorService @Inject constructor(@ApplicationContext private val co
         data = floatArray,
         numThreads = 8
     ).convertTranscription().joinToString(separator = " ") { it.text }
+
+    suspend fun File.processAudioSegments(
+        context: Context,
+        segmentDurationSeconds: Int = 30,
+        overlapSeconds: Int = 2,
+        onSegmentProcessed: suspend (
+            floatArray: FloatArray,
+            progress: Int,
+            startTimeSeconds: Long
+        ) -> Unit
+    ) {
+        val wavFile = this.resample(format = AudioResampler.OutputFormat.WAV, context = context)
+        val bytesPerSecond = 16000 * 2
+        val inputStream = wavFile.inputStream()
+
+        inputStream.skip(44)
+        processAudioSegments(
+            bytesPerSecond * segmentDurationSeconds,
+            bytesPerSecond * overlapSeconds,
+            wavFile.length() - 44,
+            inputStream,
+            onProcess = { totalSegments, segmentNumber, startTime, floatArray ->
+                val progress = (segmentNumber.toFloat() / totalSegments * 100).toInt()
+                val startTimeSeconds = ((startTime.toFloat() / bytesPerSecond) * 1000).toLong()
+
+                onSegmentProcessed(floatArray, progress, startTimeSeconds)
+            }
+        )
+        this.resample(format = AudioResampler.OutputFormat.MP3, context = context)
+            .moveTo(this.absoluteFile)
+
+        wavFile.delete()
+        inputStream.close()
+    }
+
+    suspend fun processAudioSegments(
+        segmentSize: Int,
+        overlapSize: Int,
+        audioSize: Long,
+        inputStream: FileInputStream,
+        onProcess: suspend (
+            totalSegments: Int,
+            segmentNumber: Int,
+            startTime: Long,
+            segmentProcessed: FloatArray
+        ) -> Unit
+    ) {
+        var segmentNumber = 0
+        var currentPositionBytes = 0L
+        var previousOverlap = ByteArray(0)
+        val totalSegments =
+            ((audioSize - overlapSize) / (segmentSize - overlapSize).toFloat()).toInt() + 1
+
+        while (currentPositionBytes < audioSize) {
+            segmentNumber++
+
+            val bytesToRead = minOf(
+                segmentSize - previousOverlap.size,
+                (audioSize - currentPositionBytes).toInt()
+            )
+            val buffer = ByteArray(bytesToRead)
+            val bytesRead = inputStream.read(buffer)
+
+            if (bytesRead == -1) break
+
+            val fullSegment = if (previousOverlap.isNotEmpty()) {
+                previousOverlap + buffer.copyOf(bytesRead)
+            } else {
+                buffer.copyOf(bytesRead)
+            }
+            val floatArray = normalizeAudio(fullSegment)
+            val startTime = if (segmentNumber ==1) {
+                0L
+            } else {
+                currentPositionBytes - previousOverlap.size
+            }
+
+            onProcess(totalSegments, segmentNumber, startTime, floatArray)
+
+            val overlapStartByte = maxOf(0, fullSegment.size - overlapSize)
+
+            previousOverlap = fullSegment.copyOfRange(overlapStartByte, fullSegment.size)
+            currentPositionBytes += bytesRead
+        }
+    }
+
+    fun normalizeAudio(byteArray: ByteArray): FloatArray {
+        val floatArray = FloatArray(byteArray.size / 2)
+        val shortBuffer = ByteBuffer.wrap(byteArray)
+            .order(ByteOrder.LITTLE_ENDIAN)
+            .asShortBuffer()
+
+        for (i in 0 until shortBuffer.limit()) {
+            floatArray[i] = (shortBuffer.get(i) / 32767.0f).coerceIn(-1f..1f)
+        }
+
+        return floatArray
+    }
 }
--- app/src/main/java/io/github/vinnih/kipty/utils/FileUtils.kt
+++ app/src/main/java/io/github/vinnih/kipty/utils/FileUtils.kt
@@ -1,12 +1,7 @@
 package io.github.vinnih.kipty.utils
 
-import android.content.Context
-import io.github.vinnih.kipty.utils.AudioResampler.resample
 import java.io.File
-import java.io.FileInputStream
 import java.io.InputStream
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
 
 fun File.createFile(): File {
     if (!this.exists()) {
@@ -24,100 +19,6 @@ fun File.createFolder(): File {
     return this
 }
 
-suspend fun File.processAudioSegments(
-    context: Context,
-    segmentDurationSeconds: Int = 30,
-    overlapSeconds: Int = 2,
-    onSegmentProcessed: suspend (
-        floatArray: FloatArray,
-        progress: Int,
-        startTimeSeconds: Long
-    ) -> Unit
-) {
-    val wavFile = this.resample(format = AudioResampler.OutputFormat.WAV, context = context)
-    val bytesPerSecond = 16000 * 2
-    val inputStream = wavFile.inputStream()
-
-    inputStream.skip(44)
-    processAudioSegments(
-        bytesPerSecond * segmentDurationSeconds,
-        bytesPerSecond * overlapSeconds,
-        wavFile.length() - 44,
-        inputStream,
-        onProcess = { totalSegments, segmentNumber, startTime, floatArray ->
-            val progress = (segmentNumber.toFloat() / totalSegments * 100).toInt()
-            val startTimeSeconds = ((startTime.toFloat() / bytesPerSecond) * 1000).toLong()
-
-            onSegmentProcessed(floatArray, progress, startTimeSeconds)
-        }
-    )
-    this.resample(format = AudioResampler.OutputFormat.MP3, context = context)
-        .moveTo(this.absoluteFile)
-
-    wavFile.delete()
-    inputStream.close()
-}
-
-suspend fun processAudioSegments(
-    segmentSize: Int,
-    overlapSize: Int,
-    audioSize: Long,
-    inputStream: FileInputStream,
-    onProcess: suspend (
-        totalSegments: Int,
-        segmentNumber: Int,
-        startTime: Long,
-        segmentProcessed: FloatArray
-    ) -> Unit
-) {
-    var segmentNumber = 0
-    var currentPositionBytes = 0L
-    var previousOverlap = ByteArray(0)
-    val totalSegments =
-        ((audioSize - overlapSize) / (segmentSize - overlapSize).toFloat()).toInt() + 1
-
-    while (currentPositionBytes < audioSize) {
-        segmentNumber++
-
-        val bytesToRead = minOf(
-            segmentSize - previousOverlap.size,
-            (audioSize - currentPositionBytes).toInt()
-        )
-        val buffer = ByteArray(bytesToRead)
-        val bytesRead = inputStream.read(buffer)
-
-        if (bytesRead == -1) break
-
-        val fullSegment = if (previousOverlap.isNotEmpty()) {
-            previousOverlap + buffer.copyOf(bytesRead)
-        } else {
-            buffer.copyOf(bytesRead)
-        }
-        val floatArray = normalizeAudio(fullSegment)
-        val startTime = if (segmentNumber == 1) 0L else currentPositionBytes - previousOverlap.size
-
-        onProcess(totalSegments, segmentNumber, startTime, floatArray)
-
-        val overlapStartByte = maxOf(0, fullSegment.size - overlapSize)
-
-        previousOverlap = fullSegment.copyOfRange(overlapStartByte, fullSegment.size)
-        currentPositionBytes += bytesRead
-    }
-}
-
-fun normalizeAudio(byteArray: ByteArray): FloatArray {
-    val floatArray = FloatArray(byteArray.size / 2)
-    val shortBuffer = ByteBuffer.wrap(byteArray)
-        .order(ByteOrder.LITTLE_ENDIAN)
-        .asShortBuffer()
-
-    for (i in 0 until shortBuffer.limit()) {
-        floatArray[i] = (shortBuffer.get(i) / 32767.0f).coerceIn(-1f..1f)
-    }
-
-    return floatArray
-}
-
 fun InputStream.copyTo(file: File) {
     file.outputStream().use { outputStream ->
         this.copyTo(outputStream)
@@ -127,11 +28,4 @@ fun InputStream.copyTo(file: File) {
 fun File.moveTo(file: File) {
     this.copyTo(file, overwrite = true)
     this.deleteRecursively()
-}
-
-fun Long.getFormattedSize(): String = when {
-    this >= 1024 * 1024 * 1024 -> "%.1f GB".format(this / (1024.0 * 1024.0 * 1024.0))
-    this >= 1024 * 1024 -> "%.1f MB".format(this / (1024.0 * 1024.0))
-    this >= 1024 -> "%.1f KB".format(this / 1024.0)
-    else -> "$this Bytes"
 }
